cfcb654b427ff417f955fc135e4acc62ead3bc33
daliikbel <162151519+daliikbel@users.noreply.github.com>
2024-03-28 10:27:59 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./Shopping.html","./Homme.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
